443d300c8e4937082e934fd63ae77dee0108db7c
cjperes <caio@bbcreative.org>
2018-09-27 17:45:14 -0300

    tratamento campo telefone

--- js.js
+++ js.js
@@ -1,3 +1,15 @@
 function go() {
         document.getElementById("result").innerHTML = "https://api.whatsapp.com/send?phone=55" + document.getElementById('tel').value + "&text=" + document.getElementById('msg').value;
     }
+
+
+    function SomenteNumero(e){
+        var tecla=(window.event)?event.keyCode:e.which;   
+        if((tecla>47 && tecla<58)) return true;
+        else{
+            if (tecla==8 || tecla==0) return true;
+        else  return false;
+        }
+    }
+    
+  
